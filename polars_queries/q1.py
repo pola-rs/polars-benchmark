d5b9dad37437eb8ad61725b1d16653549473dead
Ritchie Vink <ritchie46@gmail.com>
2022-06-12 11:48:17 +0200

    reuse more code

--- polars_queries/q1.py
+++ polars_queries/q1.py
@@ -4,37 +4,40 @@ from datetime import datetime
 
 import polars as pl
 
-os.environ["POLARS_VERBOSE"] = "1"
+from polars_queries import polars_tpch_utils
 
-q = pl.scan_parquet(
-    "tables_scale_1/lineitem.parquet",
-)
+Q_NUM = 1
 
-q = (
-    q.filter(pl.col("l_shipdate") <= datetime(1998, 12, 1))
-    .groupby(["l_returnflag", "l_linestatus"])
-    .agg(
-        [
-            pl.sum("l_quantity").alias("sum_qty"),
-            pl.sum("l_extendedprice").alias("sum_base_price"),
-            (pl.col("l_extendedprice") * (1 - pl.col("l_discount")))
-            .sum()
-            .alias("sum_disc_price"),
-            (
-                pl.col("l_extendedprice")
-                * (1.0 - pl.col("l_discount") * (1.0 - pl.col("l_tax")))
-            )
-            .sum()
-            .alias("sum_charge"),
-            pl.mean("l_quantity").alias("avg_qty"),
-            pl.mean("l_extendedprice").alias("avg_price"),
-            pl.mean("l_discount").alias("avg_disc"),
-            pl.count().alias("count_order"),
-        ],
+
+def q():
+    q = polars_tpch_utils.get_line_item_ds()
+    q_final = (
+        q.filter(pl.col("l_shipdate") <= datetime(1998, 12, 1))
+        .groupby(["l_returnflag", "l_linestatus"])
+        .agg(
+            [
+                pl.sum("l_quantity").alias("sum_qty"),
+                pl.sum("l_extendedprice").alias("sum_base_price"),
+                (pl.col("l_extendedprice") * (1 - pl.col("l_discount")))
+                .sum()
+                .alias("sum_disc_price"),
+                (
+                    pl.col("l_extendedprice")
+                    * (1.0 - pl.col("l_discount") * (1.0 - pl.col("l_tax")))
+                )
+                .sum()
+                .alias("sum_charge"),
+                pl.mean("l_quantity").alias("avg_qty"),
+                pl.mean("l_extendedprice").alias("avg_price"),
+                pl.mean("l_discount").alias("avg_disc"),
+                pl.count().alias("count_order"),
+            ],
+        )
+        .sort(["l_returnflag", "l_linestatus"])
     )
-    .sort(["l_returnflag", "l_linestatus"])
-)
 
-t0 = time.time()
-print(q.collect())
-print(time.time() - t0)
+    polars_tpch_utils.run_query(Q_NUM, q_final)
+
+
+if __name__ == "__main__":
+    q()
--- polars_queries/q4.py
+++ polars_queries/q4.py
@@ -1,14 +1,12 @@
 from datetime import datetime
 
 import polars as pl
-from linetimer import CodeTimer, linetimer
 
 from polars_queries import polars_tpch_utils
 
 Q_NUM = 4
 
 
-@linetimer(name=f"Overall execution of Query {Q_NUM}", unit="s")
 def q():
     var1 = datetime(1993, 7, 1)
     var2 = datetime(1993, 10, 1)
@@ -16,21 +14,19 @@ def q():
     line_item_ds = polars_tpch_utils.get_line_item_ds()
     orders_ds = polars_tpch_utils.get_orders_ds()
 
-    with CodeTimer(name=f"Get result of Query {Q_NUM}", unit="s"):
-        result_df = (
-            line_item_ds.join(orders_ds, left_on="l_orderkey", right_on="o_orderkey")
-            .filter(pl.col("o_orderdate") >= var1)
-            .filter(pl.col("o_orderdate") < var2)
-            .filter(pl.col("l_commitdate") < pl.col("l_receiptdate"))
-            .unique(subset=["o_orderpriority", "l_orderkey"])
-            .groupby("o_orderpriority")
-            .agg(pl.count().alias("order_count"))
-            .sort(by="o_orderpriority")
-            .with_column(pl.col("order_count").cast(pl.datatypes.Int64))
-        ).collect()
+    q_final = (
+        line_item_ds.join(orders_ds, left_on="l_orderkey", right_on="o_orderkey")
+        .filter(pl.col("o_orderdate") >= var1)
+        .filter(pl.col("o_orderdate") < var2)
+        .filter(pl.col("l_commitdate") < pl.col("l_receiptdate"))
+        .unique(subset=["o_orderpriority", "l_orderkey"])
+        .groupby("o_orderpriority")
+        .agg(pl.count().alias("order_count"))
+        .sort(by="o_orderpriority")
+        .with_column(pl.col("order_count").cast(pl.datatypes.Int64))
+    )
 
-        print(result_df)
-    polars_tpch_utils.test_results(Q_NUM, result_df)
+    polars_tpch_utils.run_query(Q_NUM, q_final)
 
 
 if __name__ == "__main__":
