e86b858a8535e2a109450b332c5b140f8c7f0749
Ritchie Vink <ritchie46@gmail.com>
2022-06-16 11:25:50 +0200

    working dask q2

--- utils.py
+++ utils.py
@@ -3,8 +3,9 @@ import os
 INCLUDE_IO = bool(os.environ.get("INCLUDE_IO", False))
 print("include io:", INCLUDE_IO)
 
-__default_dataset_base_dir = "tables_scale_1"
-__default_answers_base_dir = "tpch-dbgen/answers"
+CWD = os.path.dirname(os.path.realpath(__file__))
+__default_dataset_base_dir = os.path.join(CWD, "tables_scale_1")
+__default_answers_base_dir = os.path.join(CWD, "tpch-dbgen/answers")
 
 
 def on_second_call(func):
@@ -13,12 +14,11 @@ def on_second_call(func):
 
         # first call is outside the function
         # this call must set the result
-
         if helper.calls == 1:
             # include IO will compute the result on the 2nd call
             if not INCLUDE_IO:
                 helper.result = func(*args, **kwargs)
-            return None
+            return helper.result
 
         # second call is in the query, now we set the result
         if INCLUDE_IO and helper.calls == 2:
