b0f0ddb73bf2307bd6f59c615ee6287b8ddeb854
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-02-21 03:40:56 +0100

    WIP

--- polars_queries/q1.py
+++ polars_queries/q1.py
@@ -12,26 +12,24 @@ def q():
     q = utils.get_line_item_ds()
     q_final = (
         q.filter(pl.col("l_shipdate") <= var_1)
-        .group_by(["l_returnflag", "l_linestatus"])
+        .group_by("l_returnflag", "l_linestatus")
         .agg(
-            [
-                pl.sum("l_quantity").alias("sum_qty"),
-                pl.sum("l_extendedprice").alias("sum_base_price"),
-                (pl.col("l_extendedprice") * (1 - pl.col("l_discount")))
-                .sum()
-                .alias("sum_disc_price"),
-                (
-                    pl.col("l_extendedprice")
-                    * (1.0 - pl.col("l_discount"))
-                    * (1.0 + pl.col("l_tax"))
-                )
-                .sum()
-                .alias("sum_charge"),
-                pl.mean("l_quantity").alias("avg_qty"),
-                pl.mean("l_extendedprice").alias("avg_price"),
-                pl.mean("l_discount").alias("avg_disc"),
-                pl.len().alias("count_order"),
-            ],
+            pl.sum("l_quantity").alias("sum_qty"),
+            pl.sum("l_extendedprice").alias("sum_base_price"),
+            (pl.col("l_extendedprice") * (1 - pl.col("l_discount")))
+            .sum()
+            .alias("sum_disc_price"),
+            (
+                pl.col("l_extendedprice")
+                * (1.0 - pl.col("l_discount"))
+                * (1.0 + pl.col("l_tax"))
+            )
+            .sum()
+            .alias("sum_charge"),
+            pl.mean("l_quantity").alias("avg_qty"),
+            pl.mean("l_extendedprice").alias("avg_price"),
+            pl.mean("l_discount").alias("avg_disc"),
+            pl.len().alias("count_order"),
         )
         .sort(["l_returnflag", "l_linestatus"])
     )
