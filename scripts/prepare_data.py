88e9ca4a25fcf93150a33bacf8001ae885b0dfa9
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-02-22 21:08:47 +0100

    Refactor data prep

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -4,107 +4,104 @@ from pathlib import Path
 import polars as pl
 
 SCALE_FACTOR = int(sys.argv[1])
+
 ROOT_DIR = Path(__file__).parent.parent
 TABLES_DIR = ROOT_DIR / "data" / "tables" / f"scale-{SCALE_FACTOR}"
 
-h_nation = [
-    "n_nationkey",
-    "n_name",
-    "n_regionkey",
-    "n_comment",
-]
-h_region = [
-    "r_regionkey",
-    "r_name",
-    "r_comment",
-]
-h_part = [
-    "p_partkey",
-    "p_name",
-    "p_mfgr",
-    "p_brand",
-    "p_type",
-    "p_size",
-    "p_container",
-    "p_retailprice",
-    "p_comment",
-]
-h_supplier = [
-    "s_suppkey",
-    "s_name",
-    "s_address",
-    "s_nationkey",
-    "s_phone",
-    "s_acctbal",
-    "s_comment",
-]
-h_partsupp = [
-    "ps_partkey",
-    "ps_suppkey",
-    "ps_availqty",
-    "ps_supplycost",
-    "ps_comment",
-]
-h_customer = [
-    "c_custkey",
-    "c_name",
-    "c_address",
-    "c_nationkey",
-    "c_phone",
-    "c_acctbal",
-    "c_mktsegment",
-    "c_comment",
-]
-h_orders = [
-    "o_orderkey",
-    "o_custkey",
-    "o_orderstatus",
-    "o_totalprice",
-    "o_orderdate",
-    "o_orderpriority",
-    "o_clerk",
-    "o_shippriority",
-    "o_comment",
-]
-h_lineitem = [
-    "l_orderkey",
-    "l_partkey",
-    "l_suppkey",
-    "l_linenumber",
-    "l_quantity",
-    "l_extendedprice",
-    "l_discount",
-    "l_tax",
-    "l_returnflag",
-    "l_linestatus",
-    "l_shipdate",
-    "l_commitdate",
-    "l_receiptdate",
-    "l_shipinstruct",
-    "l_shipmode",
-    "comments",
-]
+table_columns = {
+    "customer": [
+        "c_custkey",
+        "c_name",
+        "c_address",
+        "c_nationkey",
+        "c_phone",
+        "c_acctbal",
+        "c_mktsegment",
+        "c_comment",
+    ],
+    "lineitem": [
+        "l_orderkey",
+        "l_partkey",
+        "l_suppkey",
+        "l_linenumber",
+        "l_quantity",
+        "l_extendedprice",
+        "l_discount",
+        "l_tax",
+        "l_returnflag",
+        "l_linestatus",
+        "l_shipdate",
+        "l_commitdate",
+        "l_receiptdate",
+        "l_shipinstruct",
+        "l_shipmode",
+        "comments",
+    ],
+    "nation": [
+        "n_nationkey",
+        "n_name",
+        "n_regionkey",
+        "n_comment",
+    ],
+    "orders": [
+        "o_orderkey",
+        "o_custkey",
+        "o_orderstatus",
+        "o_totalprice",
+        "o_orderdate",
+        "o_orderpriority",
+        "o_clerk",
+        "o_shippriority",
+        "o_comment",
+    ],
+    "part": [
+        "p_partkey",
+        "p_name",
+        "p_mfgr",
+        "p_brand",
+        "p_type",
+        "p_size",
+        "p_container",
+        "p_retailprice",
+        "p_comment",
+    ],
+    "partsupp": [
+        "ps_partkey",
+        "ps_suppkey",
+        "ps_availqty",
+        "ps_supplycost",
+        "ps_comment",
+    ],
+    "region": [
+        "r_regionkey",
+        "r_name",
+        "r_comment",
+    ],
+    "supplier": [
+        "s_suppkey",
+        "s_name",
+        "s_address",
+        "s_nationkey",
+        "s_phone",
+        "s_acctbal",
+        "s_comment",
+    ],
+}
 
 
-for name in [
-    "nation",
-    "region",
-    "part",
-    "supplier",
-    "partsupp",
-    "customer",
-    "orders",
-    "lineitem",
-]:
-    print(f"Processing table: {name}")
+for table_name, columns in table_columns.items():
+    print(f"Processing table: {table_name}")
 
     lf = pl.scan_csv(
-        TABLES_DIR / f"{name}.tbl",
+        TABLES_DIR / f"{table_name}.tbl",
         has_header=False,
         separator="|",
         try_parse_dates=True,
-        new_columns=eval(f"h_{name}"),
+        new_columns=columns,
     )
 
-    lf.sink_parquet(TABLES_DIR / f"{name}.parquet")
-    lf.sink_ipc(TABLES_DIR / f"{name}.ipc")
+    # Drop empty last column because CSV ends with a separator
+    lf = lf.select(columns)
+
+    lf.sink_parquet(TABLES_DIR / f"{table_name}.parquet")
+    lf.sink_ipc(TABLES_DIR / f"{table_name}.ipc")
