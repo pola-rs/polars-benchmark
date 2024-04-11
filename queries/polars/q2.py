643720ba2ba0d5295f3272a1bc2d30c480e78935
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-04-11 13:28:36 +0200

    q2

--- queries/polars/q2.py
+++ queries/polars/q2.py
@@ -6,42 +6,40 @@ Q_NUM = 2
 
 
 def q() -> None:
-    var_1 = 15
-    var_2 = "BRASS"
-    var_3 = "EUROPE"
-
     region_ds = utils.get_region_ds()
     nation_ds = utils.get_nation_ds()
     supplier_ds = utils.get_supplier_ds()
     part_ds = utils.get_part_ds()
     part_supp_ds = utils.get_part_supp_ds()
 
+    var1 = 15
+    var2 = "BRASS"
+    var3 = "EUROPE"
+
     result_q1 = (
         part_ds.join(part_supp_ds, left_on="p_partkey", right_on="ps_partkey")
         .join(supplier_ds, left_on="ps_suppkey", right_on="s_suppkey")
         .join(nation_ds, left_on="s_nationkey", right_on="n_nationkey")
         .join(region_ds, left_on="n_regionkey", right_on="r_regionkey")
-        .filter(pl.col("p_size") == var_1)
-        .filter(pl.col("p_type").str.ends_with(var_2))
-        .filter(pl.col("r_name") == var_3)
+        .filter(pl.col("p_size") == var1)
+        .filter(pl.col("p_type").str.ends_with(var2))
+        .filter(pl.col("r_name") == var3)
     )
-
-    final_cols = [
-        "s_acctbal",
-        "s_name",
-        "n_name",
-        "p_partkey",
-        "p_mfgr",
-        "s_address",
-        "s_phone",
-        "s_comment",
-    ]
 
     q_final = (
         result_q1.group_by("p_partkey")
         .agg(pl.min("ps_supplycost"))
         .join(result_q1, on=["p_partkey", "ps_supplycost"])
-        .select(final_cols)
+        .select(
+            "s_acctbal",
+            "s_name",
+            "n_name",
+            "p_partkey",
+            "p_mfgr",
+            "s_address",
+            "s_phone",
+            "s_comment",
+        )
         .sort(
             by=["s_acctbal", "n_name", "s_name", "p_partkey"],
             descending=[True, False, False, False],
