565d5722f4fbb553b428ac1fc96a8609af2a2edc
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-04-12 13:42:02 +0200

    Fix q6

--- queries/dask/q6.py
+++ queries/dask/q6.py
@@ -25,16 +25,12 @@ def q() -> None:
         var4 = 0.07
         var5 = 24
 
-        flineitem = line_item_ds[
+        filt = line_item_ds[
             (line_item_ds["l_shipdate"] >= var1) & (line_item_ds["l_shipdate"] < var2)
         ]
-        flineitem = line_item_ds[
-            (line_item_ds["l_discount"] >= var3) & (line_item_ds["l_discount"] <= var4)
-        ]
-        flineitem = line_item_ds[line_item_ds["l_quantity"] < var5]
-        result_value = (
-            (flineitem["l_extendedprice"] * flineitem["l_discount"]).sum().compute()
-        )
+        filt = filt[(filt["l_discount"] >= var3) & (filt["l_discount"] <= var4)]
+        filt = filt[filt["l_quantity"] < var5]
+        result_value = (filt["l_extendedprice"] * filt["l_discount"]).sum().compute()
         result_df = pd.DataFrame({"revenue": [result_value]})
 
         return result_df
