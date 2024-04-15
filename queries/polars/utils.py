510ca76fb8174562f01fd34e3be1e0ed549a49ee
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-04-15 08:40:05 +0200

    Latest Polars version (#110)

--- queries/polars/utils.py
+++ queries/polars/utils.py
@@ -1,4 +1,3 @@
-import os
 from functools import partial
 
 import polars as pl
@@ -7,10 +6,6 @@ from queries.common_utils import check_query_result_pl, run_query_generic
 from settings import Settings
 
 settings = Settings()
-
-os.environ["POLARS_NO_STREAMING_GROUPBY"] = str(
-    int(not settings.run.polars_streaming_groupby)
-)
 
 
 def _scan_ds(table_name: str) -> pl.LazyFrame:
@@ -72,6 +67,8 @@ def run_query(query_number: int, lf: pl.LazyFrame) -> None:
         print(lf.explain(streaming=streaming, optimized=eager))
 
     query = partial(lf.collect, streaming=streaming, no_optimization=eager)
+
+    library_name = "polars" if not eager else "polars-eager"
     run_query_generic(
-        query, query_number, "polars", query_checker=check_query_result_pl
+        query, query_number, library_name, query_checker=check_query_result_pl
     )
