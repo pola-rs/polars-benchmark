27215e614ab3b4d3be502dc5aa7e96207241a82b
Ritchie Vink <ritchie46@gmail.com>
2024-06-15 17:05:44 +0200

    count supplier (#132)

--- queries/polars/q21.py
+++ queries/polars/q21.py
@@ -15,8 +15,8 @@ def q() -> None:
 
     q1 = (
         lineitem.group_by("l_orderkey")
-        .agg(pl.col("l_suppkey").n_unique().alias("nunique_col"))
-        .filter(pl.col("nunique_col") > 1)
+        .agg(pl.col("l_suppkey").len().alias("n_supp_by_order"))
+        .filter(pl.col("n_supp_by_order") > 1)
         .join(
             lineitem.filter(pl.col("l_receiptdate") > pl.col("l_commitdate")),
             on="l_orderkey",
@@ -25,12 +25,12 @@ def q() -> None:
 
     q_final = (
         q1.group_by("l_orderkey")
-        .agg(pl.col("l_suppkey").n_unique().alias("nunique_col"))
+        .agg(pl.col("l_suppkey").len().alias("n_supp_by_order"))
         .join(q1, on="l_orderkey")
         .join(supplier, left_on="l_suppkey", right_on="s_suppkey")
         .join(nation, left_on="s_nationkey", right_on="n_nationkey")
         .join(orders, left_on="l_orderkey", right_on="o_orderkey")
-        .filter(pl.col("nunique_col") == 1)
+        .filter(pl.col("n_supp_by_order") == 1)
         .filter(pl.col("n_name") == var1)
         .filter(pl.col("o_orderstatus") == "F")
         .group_by("s_name")
