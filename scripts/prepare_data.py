d4532d7cfb59ee388eb75f58e7c2a07facd03066
Robin <auc2yz7ab@mozmail.com>
2024-08-27 16:58:01 +0200

    Align comment in data prep file (#134)

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -1,19 +1,19 @@
-# Certain portions of the contents of this file are derived from TPC-H version 3.0.1
-# (retrieved from
-# http://www.tpc.org/tpc_documents_current_versions/current_specifications5.asp).
-# Such portions are subject to copyrights held by
-# Transaction Processing Performance Council (“TPC”)
-# and licensed under the TPC EULA (a copy of which accompanies this
-# file as “TPC EULA” and is also available at
-# http://www.tpc.org/tpc_documents_current_versions/current_specifications5.asp)
-# (the “TPC EULA”).
-#
-# You may not use this file except in compliance with the TPC EULA.
-# DISCLAIMER: Portions of this file is derived from the TPC-H
-# Benchmark and as such any result obtained using this file are not
-# comparable to published TPC-H Benchmark results, as the results
-# obtained from using this file do not comply with the TPC-H Benchmark.
+"""Disclaimer.
 
+Certain portions of the contents of this file are derived from TPC-H version 3.0.1
+(retrieved from
+http://www.tpc.org/tpc_documents_current_versions/current_specifications5.asp).
+Such portions are subject to copyrights held by Transaction Processing
+Performance Council (“TPC”) and licensed under the TPC EULA is available at
+http://www.tpc.org/tpc_documents_current_versions/current_specifications5.asp)
+(the “TPC EULA”).
+
+You may not use this file except in compliance with the TPC EULA.
+DISCLAIMER: Portions of this file is derived from the TPC-H benchmark and as
+such any result obtained using this file are not comparable to published TPC-H
+Benchmark results, as the results obtained from using this file do not comply with
+the TPC-H Benchmark.
+"""
 
 import polars as pl
 
