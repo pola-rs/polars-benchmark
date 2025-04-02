96380b60a24392ace931cf85998b8ac35bc13593
Vyas Ramasubramani <vyasr@nvidia.com>
2025-04-02 16:55:46 +0000

    Some style fixes

--- queries/dask/utils.py
+++ queries/dask/utils.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, cast
 
 import dask
 import dask.dataframe as dd
@@ -16,16 +16,14 @@ from settings import Settings
 if TYPE_CHECKING:
     from collections.abc import Callable
 
-    from dask.dataframe.core import DataFrame
-
 settings = Settings()
 
 dask.config.set(scheduler="threads")
 
 
-def read_ds(table_name: str) -> DataFrame:
+def read_ds(table_name: str) -> dd.DataFrame:
     if settings.run.io_type == "skip":
-        # TODO: Load into memory before returning the Dask DataFrame.
+        # TODO: Load into memory before returning the Dask dd.DataFrame.
         # Code below is tripped up by date types
         # df = pd.read_parquet(path, dtype_backend="pyarrow")
         # return dd.from_pandas(df, npartitions=os.cpu_count())
@@ -35,55 +33,55 @@ def read_ds(table_name: str) -> DataFrame:
     path = get_table_path(table_name)
 
     if settings.run.io_type == "parquet":
-        return dd.read_parquet(path, dtype_backend="pyarrow")  # type: ignore[attr-defined,no-any-return]
+        return cast("dd.DataFrame", dd.read_parquet(path, dtype_backend="pyarrow"))
     elif settings.run.io_type == "csv":
-        df = dd.read_csv(path, dtype_backend="pyarrow")  # type: ignore[attr-defined]
+        df: dd.DataFrame = dd.read_csv(path, dtype_backend="pyarrow")
         for c in df.columns:
             if c.endswith("date"):
                 df[c] = df[c].astype("date32[day][pyarrow]")
-        return df  # type: ignore[no-any-return]
+        return df
     else:
         msg = f"unsupported file type: {settings.run.io_type!r}"
         raise ValueError(msg)
 
 
 @on_second_call
-def get_line_item_ds() -> DataFrame:
+def get_line_item_ds() -> dd.DataFrame:
     return read_ds("lineitem")
 
 
 @on_second_call
-def get_orders_ds() -> DataFrame:
+def get_orders_ds() -> dd.DataFrame:
     return read_ds("orders")
 
 
 @on_second_call
-def get_customer_ds() -> DataFrame:
+def get_customer_ds() -> dd.DataFrame:
     return read_ds("customer")
 
 
 @on_second_call
-def get_region_ds() -> DataFrame:
+def get_region_ds() -> dd.DataFrame:
     return read_ds("region")
 
 
 @on_second_call
-def get_nation_ds() -> DataFrame:
+def get_nation_ds() -> dd.DataFrame:
     return read_ds("nation")
 
 
 @on_second_call
-def get_supplier_ds() -> DataFrame:
+def get_supplier_ds() -> dd.DataFrame:
     return read_ds("supplier")
 
 
 @on_second_call
-def get_part_ds() -> DataFrame:
+def get_part_ds() -> dd.DataFrame:
     return read_ds("part")
 
 
 @on_second_call
-def get_part_supp_ds() -> DataFrame:
+def get_part_supp_ds() -> dd.DataFrame:
     return read_ds("partsupp")
 
 
