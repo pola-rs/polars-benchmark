dd45af2bb3fba6c548b751bf10ff7b514d1c7bc7
Mytherin <mark.raasveldt@gmail.com>
2025-08-26 14:54:21 +0200

    Perform hot runs

--- queries/duckdb/utils.py
+++ queries/duckdb/utils.py
@@ -83,7 +83,7 @@ def run_query(query_number: int, query: str) -> None:
     else:
         def execute() -> None:
             return conn.sql(query).fetchall()
-
+    execute()
     run_query_generic(
         execute, query_number, "duckdb", query_checker=check_query_result_pl
     )
